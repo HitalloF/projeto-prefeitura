d3dec7b65a020362614333d8a28dca78424e3994
hitallof <hitalloufpe@gmail.com>
2022-08-21 17:33:20 -0300

    created eureka server

--- pagamento/src/main/java/br/com/pagamento/controller/PagamentoController.java
+++ pagamento/src/main/java/br/com/pagamento/controller/PagamentoController.java
@@ -2,8 +2,11 @@ package br.com.Pagamento;
 
 import br.com.Pagamento.entity.Pagamento;
 import br.com.Pagamento.services.PagamentoServices;
-import org.apache.coyote.Response;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class PagamentoController {
 
 
+    private static Logger logger = LoggerFactory.getLogger(PagamentoController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private PagamentoServices services;
 
 
     @GetMapping(value= "/{PessoaId}/meses/{meses}")
     public ResponseEntity<Pagamento> GetPagamento(@PathVariable Long PessoaId,@PathVariable Integer meses){
+
+        logger.info("PORT = " + env.getProperty("localhost.server.port"));
+
         Pagamento pagamento =  services.getPagamento(PessoaId, meses);
         return ResponseEntity.ok(pagamento);
     }
--- pagamento/src/main/java/br/com/pagamento/feignClients/PrefeituraFeignClient.java
+++ pagamento/src/main/java/br/com/pagamento/feignClients/PrefeituraFeignClient.java
@@ -6,8 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @Component
-@FeignClient(name= "prefeitura",url = "localhost:8001", path = "/pessoa")
+@FeignClient(name= "prefeitura", path = "/pessoa")
 public interface PrefeituraFeignClient {
     @GetMapping(value = "{id}")
     ResponseEntity<br.com.Pagamento.entity.Pessoa> findById(@PathVariable("id") Long id);
